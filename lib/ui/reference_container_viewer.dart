a0e0849399d4fb55defba5d8080af792321b6ef6
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-09-01 19:49:44 +0100

    Updated incase empty

--- lib/ui/reference_container_viewer.dart
+++ lib/ui/reference_container_viewer.dart
@@ -20,49 +20,58 @@ class _ReferenceContainerViewerState extends State<ReferenceContainerViewer> {
         title: const Text("Reference Number Selector"),
       ),
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: Settings.referenceContainers.length,
-          itemBuilder: (context, index) => Slidable(
-            actionPane: const SlidableDrawerActionPane(),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  child: Text(
-                    Settings.referenceContainers[index].info,
-                    style: TextStyle(
-                        color: Theme.of(context).textTheme.bodyText1!.color),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => ReferenceNumberViewer(
-                          rN: Settings.referenceContainers[index].refNumbers,
-                        ),
-                      ),
-                    );
-                  },
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(20),
-                  ),
-                )
-              ],
-            ),
-            secondaryActions: [
-              IconSlideAction(
-                caption: "Remove",
-                color: Colors.red,
-                icon: Icons.delete,
-                onTap: () async {
-                  setState(() {
-                    Settings.referenceContainers.removeAt(index);
-                  });
-                  await Settings.updateRefContainers();
-                },
+        child: Settings.referenceContainers.isEmpty
+            ? const  Center(
+                child: Text(
+                  "Oops, you haven't made any tickets yet",
+                  textAlign: TextAlign.center,
+                ),
               )
-            ],
-          ),
-        ),
+            : ListView.builder(
+                itemCount: Settings.referenceContainers.length,
+                itemBuilder: (context, index) => Slidable(
+                  actionPane: const SlidableDrawerActionPane(),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        child: Text(
+                          Settings.referenceContainers[index].info,
+                          style: TextStyle(
+                              color:
+                                  Theme.of(context).textTheme.bodyText1!.color),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => ReferenceNumberViewer(
+                                rN: Settings
+                                    .referenceContainers[index].refNumbers,
+                              ),
+                            ),
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(20),
+                        ),
+                      )
+                    ],
+                  ),
+                  secondaryActions: [
+                    IconSlideAction(
+                      caption: "Remove",
+                      color: Colors.red,
+                      icon: Icons.delete,
+                      onTap: () async {
+                        setState(() {
+                          Settings.referenceContainers.removeAt(index);
+                        });
+                        await Settings.updateRefContainers();
+                      },
+                    )
+                  ],
+                ),
+              ),
       ),
     );
   }
